fd529e06ac8d01f7755f2f50f7ada209dfeb0f86
Dale MA <DaleMA@Dales-MacBook-Pro.local>
2014-10-27 17:49:55 +0000

    Added divisible by 5,15 and says methods

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,16 +11,44 @@ describe Fizzbuzz do
 		end
 
 		it '5' do
-
+			expect(fizzbuzz.is_divisible_by_five?(5)).to be true
 		end
 
+		it '15' do
+			expect(fizzbuzz.is_divisible_by_fifteen?(15)).to be true
+		end
 	end
 
 	context 'knows when a number is not divisible by' do
 		it '3' do
 			expect(fizzbuzz.is_divisible_by_three?(1)).to be false
 		end
+
+		it '5' do
+			expect(fizzbuzz.is_divisible_by_five?(1)).to be false
+		end
+
+		it '15' do
+			expect(fizzbuzz.is_divisible_by_fifteen?(1)).to be false
+		end
+
+
 	end
 
+	context 'when a multiple of' do
+
+		it "3 say Fizz" do
+			expect(fizzbuzz.says(3)).to eq "Fizz"
+		end
+
+		it "5 say Buzz" do
+			expect(fizzbuzz.says(5)).to eq "Buzz"
+		end
+
+		it "15 says FizzBuzz" do
+			expect(fizzbuzz.says(15)).to eq "FizzBuzz"
+		end
+	
+	end
 
 end
