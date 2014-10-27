d7c0a74ad5880f4dc3d0e20905f4a0c62c92b797
Dale MA <DaleMA@Dales-MacBook-Pro.local>
2014-10-27 16:48:40 +0000

    Added .rb and _spec.rb files for FizzBuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,26 @@
+class FizzBuzz
+	def is_divisible_by_three?(number)
+			is_divisible_by?(number, 3)
+	end
+
+	def is_divisible_by_five?(number)
+			is_divisible_by?(number, 5)
+	end
+
+	def is_divisible_by_fifteen?(number)
+			is_divisible_by?(number, 15)
+	end
+
+	def is_divisible_by?(number, divisor)
+			number % divisor == 0
+	end
+
+def fizzbuzz(number)
+	return "FizzBuzz" if_divisible_by_fifteen?(number)
+	return "Fizz" if_divisible_by_five?(number)
+	return "Buzz" if_divisible_by_three?(number)
+	number
+
+end
+
+
